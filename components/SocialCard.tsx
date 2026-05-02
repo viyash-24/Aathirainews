6daa5144be1241ebfebd9d163d92d89f86799047
viyash-24 <viyashviyashan2417@gmail.com>
2026-05-02 21:01:20 +0530

    social page is initilize

---
+++ components/SocialCard.tsx
@@ -0,0 +1,21 @@
+interface SocialLink {
+  icon: string;
+  platform: string;
+  handle: string;
+  href: string;
+}
+
+const socialLinks: SocialLink[] = [
+  {
+    icon: "share",
+    platform: "Instagram",
+    handle: "@aathirai_news",
+    href: "#",
+  },
+  {
+    icon: "social_leaderboard",
+    platform: "Facebook",
+    handle: "AathiraiNews Official",
+    href: "#",
+  },
+];
